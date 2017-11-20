32c98a8d5f4a026c72a405f2e7d021932f27e66f
Karthik Ravi <karthikravi@Karthiks-MacBook-Pro.local>
2017-11-19 22:26:30 -0600

    The completed verions for 3 and 4  credit for part 2

--- audio/nb_bern.py
+++ audio/nb_bern.py
@@ -25,7 +25,7 @@ class BernoulliNB(object):
     def predict(self, X):
         return np.argmax(self.predict_log_proba(X), axis=1)
 
-def feature_tuner(training_data):
+def feature_tuner(training_data, length2, width):
     new_text=open(training_data, "r")
     count=0
     three_d_list=[]
@@ -34,7 +34,7 @@ def feature_tuner(training_data):
         new_line=line.replace("\n", "")
         parsed_line=new_line.replace(" ", "1")
         parsed_line=parsed_line.replace("%","0")
-        if (len(parsed_line)==10):
+        if (len(parsed_line)==width):
             for sym in parsed_line:
                 partial_list.append(sym)
         if (len(partial_list)>0):
@@ -50,7 +50,7 @@ def feature_tuner(training_data):
         for symbol in three_d_list[count]:
             total_train.append(symbol)
         count=count+1
-        if(count%25==0):
+        if(count%length2==0):
             train_no_data.append(total_train)
             total_train=[]
 
@@ -61,19 +61,31 @@ def feature_tuner(training_data):
 
     return train_no_data
 
-if __name__=="__main__":
+def label_extraction(data_path):
+    labels=[]
+    data_labels_train=open(data_path, "r")
+    for line in data_labels_train.readlines():
+        num_line=line.replace('\n', "")
+        number=int(num_line)
+        labels.append(number)
+
+    return labels
+
+
+
+def part_1():
     no_training=os.getcwd()+'/no/no_train.txt'
     yes_training=os.getcwd()+'/yes/yes_train.txt'
     no_testing=os.getcwd()+'/no/no_test.txt'
     yes_testing=os.getcwd()+'/yes/yes_test.txt'
 
     # Independent trainping data
-    no_train=feature_tuner(no_training)
-    yes_train=feature_tuner(yes_training)
+    no_train=feature_tuner(no_training, 25, 10)
+    yes_train=feature_tuner(yes_training, 25, 10)
 
     # Independent testing data
-    no_test=feature_tuner(no_testing)
-    yes_test=feature_tuner(yes_testing)
+    no_test=feature_tuner(no_testing, 25, 10)
+    yes_test=feature_tuner(yes_testing,25, 10)
 
     # Labels for each of the train data
     no_labels=[0]*131
@@ -107,3 +119,52 @@ if __name__=="__main__":
     print("Classification_report: "+ "\n" + str(classification_report(y_test, predictions)))
     print()
     print("Accuracy: "+str(accuracy_score(y_test, predictions)))
+
+
+def part_2():
+
+    training_data_path=os.getcwd()+'/data22/training_data.txt'
+    training_labels_path=os.getcwd()+'/data22/training_labels.txt'
+    testing_data_path=os.getcwd()+'/data22/testing_data.txt'
+    testing_labels_path=os.getcwd()+'/data22/testing_labels.txt'
+
+    # Getting the training and test data  from the data22 file
+    train_data=feature_tuner(training_data_path, 30, 13)
+    test_data=feature_tuner(testing_data_path, 30, 13)
+
+    #print(len(train_data))
+    #print(len(test_data))
+
+    # Getting the training and test labels from the data22 file
+    train_labels=label_extraction(training_labels_path)
+    test_labels=label_extraction(testing_labels_path)
+
+    #print(len(train_labels))
+    #print(len(test_labels))
+
+    X_train=np.array(train_data)
+    y_train=np.array(train_labels)
+    X_test=np.array(test_data)
+    y_test=np.array(test_labels)
+
+    # Doing the machine learning
+    nb=BernoulliNB(alpha=1).fit(X_train,y_train)
+    predictions=nb.predict(X_test)
+
+    # Tuning the predictions by adding 1
+    for i in list(range(len(predictions))):
+        predictions[i]=predictions[i]+1
+
+    # The end result
+    print("Confusion Matrix: "+ "\n" +str(confusion_matrix(y_test, predictions)))
+    print()
+    print("Classification_report: "+ "\n" + str(classification_report(y_test, predictions)))
+    print()
+    print("Accuracy: "+str(accuracy_score(y_test, predictions)))
+
+
+if __name__=="__main__":
+
+    # Depending on which part you want to run, make sure to comment out th other part
+    #part_1()
+    part_2()
